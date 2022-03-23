228209eafd23b97712af37b78cdbe8fa951aec54
jaredoong <jaredoong@gmail.com>
2022-03-24 02:28:06 +0800

    Code in RPi
    Able to publish command up to alignment of thermal object. Left subscriber to laptop to start up servo and flywheel

--- nfc_reader/nfc_reader/nfc_publisher.py
+++ nfc_reader/nfc_reader/nfc_publisher.py
@@ -1,16 +1,18 @@
-"""
-This example shows connecting to the PN532 with I2C (requires clock
-stretching support), SPI, or UART. SPI is best, it uses the most pins but
-is the most reliable and universally supported.
-After initialization, try waving various 13.56MHz RFID cards over it!
-"""
 import rclpy
 from rclpy.node import Node
-from custom_msgs.msg import Nfc
-
 import RPi.GPIO as GPIO
+import time
+import busio
+import board
+import adafruit_amg88xx
+import numpy as np
 
 from .pn532 import *
+from custom_msgs.msg import Nfc, Button, Thermal
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+
 
 class NFCFinder(Node):
 
@@ -19,7 +21,7 @@ class NFCFinder(Node):
         self.publisher_ = self.create_publisher(Nfc, 'nfc_found', 10)
         self.get_logger().info('Created publisher')
         self.get_logger().info('NFC function started')
-        self.nfc_callback()
+        # self.nfc_callback()
 
     def nfc_callback(self):
         try:
@@ -49,8 +51,81 @@ class NFCFinder(Node):
                 self.get_logger().info('NFC found')
                 msg.nfc_found = True
                 self.publisher_.publish(msg)
-                exit()
+                break
                 #self.get_logger().info('NFC found is %s' % msg.nfc_found)
+                
+        except Exception as e:
+            print(e)
+        #finally:
+        #    GPIO.cleanup()
+
+class ButtonPress(Node):
+
+    def __init__(self):
+        super().__init__('button')
+        self.publisher_ = self.create_publisher(Button, 'button_pressed', 10)
+        self.get_logger().info('Created publisher')
+        self.get_logger().info('Button function started')
+        # self.button_callback()
+
+    def button_callback(self):
+        try:
+            
+            msg = Button()
+            self.button_pressed = False
+
+            while True:
+                if GPIO.input(15) != GPIO.LOW:
+                    self.get_logger().info("Button not pressed")
+                    continue
+
+                self.get_logger().info('Button pressed')
+                msg.button_pressed = True
+                self.publisher_.publish(msg)
+                break
+                
+        except Exception as e:
+            print(e)
+        finally:
+            GPIO.cleanup()
+
+class ThermalCam(Node):
+
+    def __init__(self):
+        super().__init__('thermal_cam')
+        self.publisher_ = self.create_publisher(Thermal, 'thermal', 10)
+        self.get_logger().info('Created publisher')
+        self.get_logger().info('Thermal function started')
+
+        # Seting up the thermal sensor
+        i2c = busio.I2C(board.SCL, board.SDA)
+        self.amg = adafruit_amg88xx.AMG88XX(i2c)
+        
+        # create timer for publisher
+        #time_duration = 0.1 # in seconds
+        #self.timer = self.create_timer(time_duration, self.thermal_callback)
+
+    def thermal_callback(self):
+        try:
+            
+            msg = Thermal()
+            # for breaking out of this function to start launcher
+            self.ready = False
+
+            # Seting up the thermal sensor
+            #i2c = busio.I2C(board.SCL, board.SDA)
+            #amg = adafruit_amg88xx.AMG88XX(i2c)
+            
+            while True:
+                # Sending data to msg
+                count = 0
+                for row in self.amg.pixels:
+                    for col in row:
+                        msg.thermal[count] = col
+                        count += 1
+                self.publisher_.publish(msg)
+                self.get_logger().info('Msg published')
+                
                 
         except Exception as e:
             print(e)
@@ -60,9 +135,18 @@ class NFCFinder(Node):
 def main(args=None):
     rclpy.init(args=args)
 
+    # start with finding NFC
     nfc_pub = NFCFinder()
+    nfc_pub.nfc_callback()
+    print("Back in main loop")
 
-    rclpy.spin(nfc_pub)
+    # once NFC found, wait for button press
+    button_pub = ButtonPress()
+    button_pub.button_callback()
+
+    # start the thermal function once button pressed
+    thermal_pub = ThermalCam()
+    thermal_pub.thermal_callback()
 
     nfc_pub.destroy_node()
     rclpy.shutdown()
